8a5c4de6fc8ece8c8c686eca88f8efc42ae1670e
przemyslawjurkiewicz <42567877+przemyslawjurkiewicz@users.noreply.github.com>
2019-01-31 14:24:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var fs = require("fs");
-var colors = require("colors");
+var fs = require('fs');
+var colors = require('colors');
 
-fs.readdir("./", "utf-8", function(err, data) {
-  console.log("Pobrano zawartość katalogu.".green);
+fs.readdir('./', 'utf-8', function(err, data) {
+  console.log('Pobrano zawartość katalogu.'.green);
   var dirList = data;
-  fs.writeFile("./dirContentsList.txt", dirList.join("\n"), function(err) {
+  fs.writeFile('./dirContentsList.txt', dirList.join('\n'), function(err) {
     if (err) throw err;
-    console.log("Zapisano do pliku dirContentsList.txt !".blue);
-    fs.readFile("./dirContentsList.txt", "utf-8", function(err, data) {
-      console.log("Dane po zapisie:".red);
+    console.log('Zapisano do pliku dirContentsList.txt !'.blue);
+    fs.readFile('./dirContentsList.txt', 'utf-8', function(err, data) {
+      console.log('Dane po zapisie:'.red);
       console.log(data);
     });
   });
