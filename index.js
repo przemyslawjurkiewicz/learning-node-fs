5e1baa2956f0c658c83d1b994fc88b0ad8a23426
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-02-01 08:24:21 +0100

    Add error catch

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var fs = require('fs');
 var colors = require('colors');
 
-fs.readdir('./', 'utf-8', function(err, data) {
+fs.readdir('./', 'utf-8', function (err, data) {
+  if (err) throw err;
   console.log('Pobrano zawartość katalogu.'.green);
   var dirList = data;
-  fs.writeFile('./dirContentsList.txt', dirList.join('\n'), function(err) {
+  fs.writeFile('./dirContentsList.txt', dirList.join('\n'), function (err) {
     if (err) throw err;
     console.log('Zapisano do pliku dirContentsList.txt !'.blue);
-    fs.readFile('./dirContentsList.txt', 'utf-8', function(err, data) {
+    fs.readFile('./dirContentsList.txt', 'utf-8', function (err, data) {
       console.log('Dane po zapisie:'.red);
       console.log(data);
     });
